bacb0967ab3c1e26e91db32f803fd8f1c11bd188
rebfarish <rebfarish@gmail.com>
2018-09-08 21:00:14 -0600

    Edit of hello world commit

--- src/edu/cnm/deepdive/HelloWorld.java
+++ src/edu/cnm/deepdive/HelloWorld.java
@@ -3,7 +3,7 @@ package edu.cnm.deepdive;
 public class HelloWorld {
 
   public static void main(String[] args) {
-    System.out.println("hello-world");
-    
+    System.out.println("Hello, World!");
+
   }
 }
